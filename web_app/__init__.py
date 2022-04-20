598e2c2ba13aefb7fb07a665cffaeb0e303c7e91
annava28 <97325167+annava28@users.noreply.github.com>
2022-04-19 22:49:00 -0400

    more troubleshooting

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,7 +8,7 @@ from web_app.routes.weather_routes import weather_routes
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") #set this to a random set of numbers on Heroku
 
 def create_app():
     app = Flask(__name__)
